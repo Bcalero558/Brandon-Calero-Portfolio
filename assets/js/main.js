4dd86e85ca61d47cc0e728441d3c8ee51e14afea
Bcalero558 <118758850+Bcalero558@users.noreply.github.com>
2025-06-24 18:10:30 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,10 +116,10 @@ function drawSnake(){
 function changeDirection(event)
 {
     const keypressed = event.keyCode;
-    const LEFT = 37;
-    const UP = 38;
-    const RIGHT = 39;
-    const DOWN = 40;
+    const LEFT = 65;
+    const UP = 87;
+    const RIGHT = 68;
+    const DOWN = 83;
 
     const goingUp = (yVelocity == -unitSize)
     const goingDown = (yVelocity == unitSize)
